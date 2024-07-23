e98812be6e2e6f37913263d167f8cd6fcab1d1b8
IacobucciB <link00222@gmail.com>
2024-07-23 14:22:48 -0300

    indentation

--- mc/ej2-matriz-omp.c
+++ mc/ej2-matriz-omp.c
@@ -9,13 +9,15 @@ void imprimir_matriz(int **m, int N);
 int main(int argc, char *argv[])
 {
   // Comprobar Error de Parametros
-  if (argc != 3) {
+  if (argc != 3)
+  {
     printf("Error: No se proporciono la dimension de la Matriz y el numero de Threads \n");
     return 1;
   }
   int N = atoi(argv[1]);
   int T = atoi(argv[2]);
-  if (N <= 0 || T <= 0) {
+  if (N <= 0 || T <= 0)
+  {
     printf("Error: La dimension de la Matriz y el numero de Threads deben ser un Numero Entero Positivo \n");
     return 1;
   }
@@ -27,15 +29,18 @@ int main(int argc, char *argv[])
   int **a = (int **)malloc(N * sizeof(int *));
   int **b = (int **)malloc(N * sizeof(int *));
   int **c = (int **)malloc(N * sizeof(int *));
-  if (a == NULL || b == NULL || c == NULL) {
+  if (a == NULL || b == NULL || c == NULL)
+  {
     printf("Error: Fallo en la Asignacion de Memoria \n");
     return 1;
   }
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
+  {
     a[i] = (int *)malloc(N * sizeof(int));
     b[i] = (int *)malloc(N * sizeof(int));
     c[i] = (int *)malloc(N * sizeof(int));
-    if (a[i] == NULL || b[i] == NULL || c[i] == NULL) {
+    if (a[i] == NULL || b[i] == NULL || c[i] == NULL)
+    {
       printf("Error: Fallo en la Asignacion de Memoria \n");
       return 1;
     }
@@ -54,12 +59,15 @@ int main(int argc, char *argv[])
 #pragma omp parallel default(none) shared(a, b, c, N)
   {
 #pragma omp for
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-	c[i][j] = 0;
-	for (int k = 0; k < N; k++) {
-	  c[i][j] += a[i][k] * b[k][j];
-	}
+    for (int i = 0; i < N; i++)
+    {
+      for (int j = 0; j < N; j++)
+      {
+        c[i][j] = 0;
+        for (int k = 0; k < N; k++)
+        {
+          c[i][j] += a[i][k] * b[k][j];
+        }
       }
     }
   }
@@ -68,7 +76,8 @@ int main(int argc, char *argv[])
   imprimir_matriz(c, N);
 
   // Liberar memoria
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
+  {
     free(a[i]);
     free(b[i]);
     free(c[i]);
@@ -80,17 +89,23 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void llenar_matriz(int **m, int N) {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+void llenar_matriz(int **m, int N)
+{
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
       m[i][j] = (rand() % 5) + 1;
     }
   }
 }
 
-void imprimir_matriz(int **m, int N) {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+void imprimir_matriz(int **m, int N)
+{
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
       printf("%d ", m[i][j]);
     }
     printf("\n");
